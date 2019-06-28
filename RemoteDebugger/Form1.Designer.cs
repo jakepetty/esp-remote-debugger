4da58f7ee51b623a3ccad72976cb86645e38dbde
Jake Petty <jake.petty@vexcon.net>
2019-06-28 00:17:21 -0500

    Initial Commit

---
+++ RemoteDebugger/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.log = new System.Windows.Forms.ListView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.RebootToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.autoScroll = new System.Windows.Forms.CheckBox();
+            this.svrAddress = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(674, 446);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clear Log";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ClearLog_Click);
+            // 
+            // log
+            // 
+            this.log.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.log.AutoArrange = false;
+            this.log.ContextMenuStrip = this.contextMenuStrip1;
+            this.log.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.log.Location = new System.Drawing.Point(12, 12);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(737, 424);
+            this.log.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.log.TabIndex = 3;
+            this.log.UseCompatibleStateImageBehavior = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.RebootToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(113, 26);
+            // 
+            // RebootToolStripMenuItem
+            // 
+            this.RebootToolStripMenuItem.Name = "RebootToolStripMenuItem";
+            this.RebootToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.RebootToolStripMenuItem.Text = "Reboot";
+            this.RebootToolStripMenuItem.Click += new System.EventHandler(this.RebootToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 451);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Error: 0 Warn: 0 Info: 0 Success: 0";
+            // 
+            // autoScroll
+            // 
+            this.autoScroll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.autoScroll.AutoSize = true;
+            this.autoScroll.Location = new System.Drawing.Point(596, 450);
+            this.autoScroll.Name = "autoScroll";
+            this.autoScroll.Size = new System.Drawing.Size(72, 17);
+            this.autoScroll.TabIndex = 6;
+            this.autoScroll.Text = "Autoscroll";
+            this.autoScroll.UseVisualStyleBackColor = true;
+            this.autoScroll.CheckedChanged += new System.EventHandler(this.AutoScroll_CheckedChanged);
+            // 
+            // svrAddress
+            // 
+            this.svrAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.svrAddress.AutoSize = true;
+            this.svrAddress.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.svrAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.svrAddress.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.svrAddress.Location = new System.Drawing.Point(513, 451);
+            this.svrAddress.Name = "svrAddress";
+            this.svrAddress.Size = new System.Drawing.Size(77, 13);
+            this.svrAddress.TabIndex = 7;
+            this.svrAddress.Text = "configuration.h";
+            this.svrAddress.Click += new System.EventHandler(this.SvrAddress_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 477);
+            this.Controls.Add(this.svrAddress);
+            this.Controls.Add(this.autoScroll);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Remote Debugger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView log;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem RebootToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox autoScroll;
+        private System.Windows.Forms.Label svrAddress;
+    }
+}
+
---
+++ RemoteDebugger/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int SERVER_PORT = 4824;
+        bool AUTO_SCROLL = true;
+        String SERVER_IP;
+        int errors;
+        int warnings;
+        int successes;
+        int infos;
+        public void Server()
+        {
+            string chunk = "";
+            UdpClient server = new UdpClient(SERVER_PORT);
+            while (true)
+            {
+                // Get Client Information
+                IPEndPoint client = new IPEndPoint(IPAddress.Any, 0);
+                // Get Payload
+                string payload = Encoding.ASCII.GetString(server.Receive(ref client));
+
+                if (payload.Length > 0)
+                {
+                    // Add payload to a buffer
+                    chunk += payload;
+                    // Check if buffer contains a new line indicating the payload is ready to log
+                    if (chunk.Contains("\n"))
+                    {
+                        this.Invoke((MethodInvoker)delegate ()
+                        {
+                            // Split the payload into lines
+                            string[] chunks = chunk.Split('\n');
+                            // Loop through each line
+                            for (int i = 0; i < chunks.Length; i++)
+                            {
+                                // Remove any whitespace
+                                string line = chunks[i].Trim();
+                                // Make sure line isnt empty
+                                if (line.Length > 0)
+                                {
+                                    // Build each cell in the row
+                                    string[] cells = new string[4];
+                                    cells[0] = DateTime.Now.ToString("h:mm:ss tt"); // Time
+                                    cells[1] = String.Format("{0}:{1}", client.Address, client.Port); // Device IP and Port
+                                    cells[2] = line.Split('[', ']')[1]; // Status
+                                    cells[3] = line.Replace("[" + cells[2] + "] ", ""); // Message
+                                    // Create Row
+                                    ListViewItem lvi = new ListViewItem(cells);
+                                    switch (cells[2])
+                                    {
+                                        case "ERROR": // Line format for errors
+                                            lvi.ForeColor = System.Drawing.Color.Red;
+                                            Font f = new Font(lvi.Font, FontStyle.Bold);
+                                            lvi.Font = f;
+                                            errors++;
+                                            break;
+                                        case "OK": // Line format for success
+                                            lvi.ForeColor = System.Drawing.Color.DarkGreen;
+                                            successes++;
+                                            break;
+                                        case "WARN": // Line format for success
+                                            lvi.ForeColor = System.Drawing.Color.Orange;
+                                            warnings++;
+                                            break;
+                                        case "INFO": // Line format for JSON
+                                            lvi.ForeColor = System.Drawing.Color.LightSlateGray;
+                                            infos++;
+                                            break;
+                                    }
+                                    // Insert new row into the log
+                                    log.Items.Add(lvi);
+
+                                    // AutoScroll to the bottom
+                                    if (AUTO_SCROLL)
+                                    {
+                                        log.Items[log.Items.Count - 1].EnsureVisible();
+                                    }
+                                    label1.Text = String.Format("Error: {0} Warn: {1} Info: {2} Success: {3}", errors, warnings, infos, successes);
+                                    // Clear buffer
+                                    chunk = "";
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    SERVER_IP = ip.ToString();
+                    break;
+                }
+            }
+            // Enable Auto Scroll
+            autoScroll.Checked = AUTO_SCROLL;
+
+            // Add column header
+            log.View = View.Details;
+            log.GridLines = true;
+            log.FullRowSelect = true;
+            log.Columns.Add("Time", 75);
+            log.Columns.Add("Device", 100);
+            log.Columns.Add("Status", 60);
+            log.Columns.Add("Message", this.Width - 254 - 42);
+            log.KeyDown += ListBox1_KeyDown;
+            this.Resize += Log_Resize;
+
+            // Trick to increase row height
+            ImageList imgList = new ImageList();
+            imgList.ImageSize = new System.Drawing.Size(1, 30);
+            log.SmallImageList = imgList;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Start the Server
+            Thread client = new Thread(new ThreadStart(Server));
+            client.IsBackground = true;
+            client.Start();
+        }
+
+        // Makes the table look cleaner
+        private void Log_Resize(object sender, EventArgs e)
+        {
+            log.Columns[3].Width = this.Width - 254 - 42;
+        }
+
+        private void ClearLog_Click(object sender, EventArgs e)
+        {
+            warnings = 0;
+            infos = 0;
+            successes = 0;
+            errors = 0;
+            log.Items.Clear();
+            label1.Text = String.Format("Error: {0} Warn: {1} Info: {2} Success: {3}", errors, warnings, infos, successes);
+        }
+        private void ListBox1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                ListView.SelectedListViewItemCollection selectedItems = log.SelectedItems;
+                String text = "";
+                foreach (ListViewItem item in selectedItems)
+                {
+                    text += String.Format("[{0}]\t{1}\n", item.SubItems[2].Text, item.SubItems[3].Text);
+                }
+                Clipboard.SetText(text);
+            }
+        }
+
+        private void RebootToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ListView.SelectedListViewItemCollection selectedItems = log.SelectedItems;
+            if (selectedItems.Count > 0)
+            {
+                String device = selectedItems[0].SubItems[1].Text;
+                String ip = device.Split(':')[0].ToString();
+                int port = Int32.Parse(device.Split(':')[1]);
+                UdpClient client = new UdpClient();
+                IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ip), port);
+                client.Connect(ep);
+                byte[] b = { 0x86 };
+                client.Send(b, sizeof(byte));
+                client.Close();
+            }
+            else
+            {
+                MessageBox.Show("No device was selected", "Oops!");
+            }
+        }
+
+        private void AutoScroll_CheckedChanged(object sender, EventArgs e)
+        {
+            AUTO_SCROLL = autoScroll.Checked;
+        }
+
+        private void SvrAddress_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(String.Format("Edit your configuration.h file with the information provided below\n\n#define DEBUG_HOST \"{0}\"", SERVER_IP), "configuration.h");
+        }
+    }
+}
